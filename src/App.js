f48b89f506e2f93cf7eee2446ef754daf2f9cc42
Shaunak Turaga <shaunak@opentrack.co>
2020-09-17 09:16:29 -0400

    hello world

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { StreamApp, NotificationDropdown, FlatFeed, LikeButton, Activity, CommentField, CommentList } from 'react-activity-feed';
+import 'react-activity-feed/dist/index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <StreamApp
+        apiKey="tzeprec2m92v"
+        appId="93310"
+        token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidXNlci1vbmUifQ.5Ni9eIBqS5KxAfL9tCR92wrcYzKNez52fEXjgIkr_FM"
+          >
+        <NotificationDropdown notify />
+        <FlatFeed
+          options={{reactions: { recent: true } }}
+          notify
+          Activity={(props) =>
+              <Activity {...props}
+                Footer={() => (
+                  <div style={ {padding: '8px 16px'} }>
+                    <LikeButton {...props} />
+                    <CommentField
+                      activity={props.activity}
+                      onAddReaction={props.onAddReaction} />
+                    <CommentList activityId={props.activity.id} />
+                  </div>
+                )}
+              />
+            }
+          />
+      </StreamApp>
+    );
+  }
 }
-
 export default App;
